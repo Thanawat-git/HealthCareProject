81b34a555585496ab126385da43a298a91af2db7
Thanawat-git <buysai123gw@gmail.com>
2020-10-06 21:20:04 +0700

    Add Sec2,3

---
+++ health-care-application/src/components/Forms/Sections2/Sections2_1.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+class Sections2 extends Component {
+  render() {
+    return <div>Sections2</div>;
+  }
+}
+
+export default Sections2;
---
+++ health-care-application/src/components/Forms/Sections3/Sections3_1.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+class Sections3 extends Component {
+  render() {
+    return <div>Sections3</div>;
+  }
+}
+
+export default Sections3;
