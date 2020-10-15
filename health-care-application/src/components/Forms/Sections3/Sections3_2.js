c736c9fb749842dadc9e4ab129a7ff9a26e7c24f
Saranya <lisaaholic@gmail.com>
2020-10-15 12:49:55 +0700

    sec3

--- health-care-application/src/components/Forms/Sections3/Sections3_2.js
+++ health-care-application/src/components/Forms/Sections3/Sections3_2.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import "../form-style.css";
 
 class Sections3 extends Component {
   render() {
@@ -10,7 +9,17 @@ class Sections3 extends Component {
       <form>
         <h2>ส่วนที่ 3 ความเสี่ยงต่อโรคหัวใจและหลอดเลือด</h2> 
         <div className="question">
-          <p>3.1 ยังสูบบุหรี่ ยาเส้น หรือหยุดสูบไม่เกิน 1 ปี</p>
+        <p>3.3 ระดับน้ำตาลในเลือด ≥ 100 หรือแพทย์บอกว่าเป็นเบาหวาน</p>
+          <div className="form-check form-check-inline">
+            <input type="radio" name="3.3" id="art3.3y" value="Y" className="form-check-input"/>
+            <label htmlFor="art3.3y" className="form-check-label">ใช่</label>
+          </div>
+          <div className="form-check form-check-inline">
+            <input type="radio" name="3.3" id="art3.3n" value="N" className="form-check-input"/>
+            <label htmlFor="art3.3n" className="form-check-label">ไม่ใช่</label>
+          </div>
+
+          <p>3.4 แพทย์หรือพยาบาลบอกว่ามีไขมันในเลือดสูง</p>
           <div className="form-check form-check-inline">
             <input type="radio" name="3.1" id="art3.1y" value="Y" className="form-check-input"/>
             <label htmlFor="art3.1y" className="form-check-label">ใช่</label>
@@ -20,16 +29,6 @@ class Sections3 extends Component {
             <label htmlFor="art3.1n" className="form-check-label">ไม่ใช่</label>
           </div>
   
-          <p>3.2 ความดันโลหิต ≥ 130/85 หรือแพทย์บอกว่าเป็นความดันโลหิตสูง</p>
-          <div className="form-check form-check-inline">
-            <input type="radio" name="3.2" id="art3.2y" value="Y" className="form-check-input"/>
-            <label htmlFor="art3.2y" className="form-check-label">ใช่</label>
-          </div>
-          <div className="form-check form-check-inline">
-            <input type="radio" name="3.2" id="art3.2n" value="N" className="form-check-input"/>
-            <label htmlFor="art3.2n" className="formzzzcheck-label">ไม่ใช่</label>
-          </div>
-  
         </div>
         <div className="row justify-content-between">
           <button type="button" class="btn form-btn btn-back btn-lg">ย้อนกลับ</button>
@@ -37,7 +36,8 @@ class Sections3 extends Component {
         </div>
       </form>
       </div>
-    </div>);
+    </div>
+    );;
   }
 }
 
--- health-care-application/src/components/Forms/Sections3/Sections3_3.js
+++ health-care-application/src/components/Forms/Sections3/Sections3_3.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import "../form-style.css";
 
 class Sections3 extends Component {
   render() {
@@ -10,26 +9,26 @@ class Sections3 extends Component {
       <form>
         <h2>ส่วนที่ 3 ความเสี่ยงต่อโรคหัวใจและหลอดเลือด</h2> 
         <div className="question">
-          <p>3.1 ยังสูบบุหรี่ ยาเส้น หรือหยุดสูบไม่เกิน 1 ปี</p>
-          <div className="form-check form-check-inline">
-            <input type="radio" name="3.1" id="art3.1y" value="Y" className="form-check-input"/>
-            <label htmlFor="art3.1y" className="form-check-label">ใช่</label>
-          </div>
-          <div className="form-check form-check-inline">
-            <input type="radio" name="3.1" id="art3.1n" value="N" className="form-check-input"/>
-            <label htmlFor="art3.1n" className="form-check-label">ไม่ใช่</label>
-          </div>
-  
-          <p>3.2 ความดันโลหิต ≥ 130/85 หรือแพทย์บอกว่าเป็นความดันโลหิตสูง</p>
+        <p>3.5 เส้นรอบเอวเกินเกณฑ์</p>
           <div className="form-check form-check-inline">
             <input type="radio" name="3.2" id="art3.2y" value="Y" className="form-check-input"/>
             <label htmlFor="art3.2y" className="form-check-label">ใช่</label>
           </div>
           <div className="form-check form-check-inline">
             <input type="radio" name="3.2" id="art3.2n" value="N" className="form-check-input"/>
-            <label htmlFor="art3.2n" className="formzzzcheck-label">ไม่ใช่</label>
+            <label htmlFor="art3.2n" className="form-check-label">ไม่ใช่</label>
           </div>
   
+          <p>3.6 แพทย์บอกว่าเป็นโรคหัวใจขาดเลือดหรือ อัมพฤกษ์ อัมพาต </p>
+          <div className="form-check form-check-inline">
+            <input type="radio" name="3.3" id="art3.3y" value="Y" className="form-check-input"/>
+            <label htmlFor="art3.3y" className="form-check-label">ใช่</label>
+          </div>
+          <div className="form-check form-check-inline">
+            <input type="radio" name="3.3" id="art3.3n" value="N" className="form-check-input"/>
+            <label htmlFor="art3.3n" className="form-check-label">ไม่ใช่</label>
+          </div>
+
         </div>
         <div className="row justify-content-between">
           <button type="button" class="btn form-btn btn-back btn-lg">ย้อนกลับ</button>
@@ -37,7 +36,8 @@ class Sections3 extends Component {
         </div>
       </form>
       </div>
-    </div>);
+    </div>
+    );;
   }
 }
 
