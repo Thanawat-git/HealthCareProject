380dbe65f45303aa193242f535cd67c65be76cf0
Saranya <lisaaholic@gmail.com>
2020-10-21 23:21:02 +0700

    Edit sec3 to one page

--- health-care-application/src/components/Forms/Sections3/index.js
+++ health-care-application/src/components/Forms/Sections3/index.js
@@ -1,6 +1,3 @@
-import sec3_1 from './Sections3_1'
-import sec3_2 from './Sections3_2'
-import sec3_3 from './Sections3_3'
-import sec3_4 from './Sections3_4'
+import sec3 from './Sections3'
 
-export {sec3_1, sec3_2, sec3_3, sec3_4}
+export {sec3}
