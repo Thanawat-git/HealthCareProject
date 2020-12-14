fc7eb5728d10bda0198926b197b02dcc1d9ef25b
Thanawat-git <buysai123gw@gmail.com>
2020-12-14 11:54:31 +0700

    add icon report

--- health-care-application/src/components/admin-super/content/reports/ReportMenuBox.js
+++ health-care-application/src/components/admin-super/content/reports/ReportMenuBox.js
@@ -1,6 +1,16 @@
 import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import BodySize from "../../../images/weight-loss.svg";
+import BMI from "../../../images/bmi.svg";
+import Heart from "../../../images/heart.svg";
+import Heart2 from "../../../images/heart2.svg";
+import Bone from "../../../images/bone.svg";
+import Brain from "../../../images/brain.svg";
+import Day from "../../../images/day.svg";
+import Eye from "../../../images/eye.svg";
+import Toilet from "../../../images/toilet.svg";
+import Bad from "../../../images/bad.svg";
+import Mouth from "../../../images/mouth.svg";
 import { Button, Paper } from "@material-ui/core";
 
 const useStyles = makeStyles((theme) => ({
@@ -18,9 +28,25 @@ export default function ReportMenuBox({ title, eachImg }) {
   return (
     <div className={classes.root}>
       <Paper className="report-box-content">
-        <h4>{title}</h4>
+        <strong><h4>{title}</h4></strong>
         <div className="report-box-content-img">
-          <img src={BodySize} />
+          {
+            eachImg=='1' ? <img src={BodySize} />:
+            eachImg=='2' ? <img src={BMI} />:
+            eachImg=='3' ? <img src={Heart2} />:
+            eachImg=='4' ? <img src={BodySize} />:
+            eachImg=='5' ? <img src={Heart} />:
+            eachImg=='6' ? <img src={Eye} />:
+            eachImg=='7' ? <img src={Mouth} />:
+            eachImg=='8' ? <img src={Day} />:
+            eachImg=='9' ? <img src={BodySize} />:
+            eachImg=='10' ? <img src={Brain} />:
+            eachImg=='11' ? <img src={BodySize} />:
+            eachImg=='12' ? <img src={Bad} />:
+            eachImg=='13' ? <img src={Bone} />:
+            <img src={Toilet} />
+          }
+          
         </div>
         <div className="report-box-content-bt">
           <Button fullWidth>ดูรายงาน</Button>
--- health-care-application/src/components/admin-super/content/reports/index.js
+++ health-care-application/src/components/admin-super/content/reports/index.js
@@ -18,15 +18,21 @@ export default function Index() {
         {/* Create Menu Here! */}
         <div className="report-box">
           <Link to={`${path}/textreport`}>
-            <ReportMenuBox title="แปลผลเส้นรอบเอว" eachImg="reportpage" />
+            <ReportMenuBox title="แปลผลเส้นรอบเอว" eachImg="1" />
           </Link>
-          <ReportMenuBox title="แปลผลเส้นรอบเอว" eachImg="reportpage" />
-          <ReportMenuBox title="แปลผลความเสี่ยงต่อโรคหัวใจและหลอดเลือด" eachImg="reportpage" />
-          <ReportMenuBox title="แปลผลเส้นรอบเอว" eachImg="reportpage" />
-          <ReportMenuBox title="แปลผลเส้นรอบเอว" eachImg="reportpage" />
-          <ReportMenuBox title="แปลผลเส้นรอบเอว" eachImg="reportpage" />
-          <ReportMenuBox title="แปลผลเส้นรอบเอว" eachImg="reportpage" />
-          <ReportMenuBox title="แปลผลเส้นรอบเอว" eachImg="reportpage" />
+          <ReportMenuBox title="ดัชนีมวลกาย (BMI)" eachImg="2" />
+          <ReportMenuBox title="ค่าความดันโลหิต" eachImg="3" />
+          <ReportMenuBox title="ค่าระดับน้ำตาลจากปลายนิ้ว" eachImg="4" />
+          <ReportMenuBox title="แปลผลความเสี่ยงต่อโรคหัวใจและหลอดเลือด" eachImg="5" />
+          <ReportMenuBox title="สุขภาพตา" eachImg="6" />
+          <ReportMenuBox title="สุขภาพช่องปาก" eachImg="7" />
+          <ReportMenuBox title="ความสามารถในการทำกิจวัตรประจำวัน" eachImg="8" />
+          <ReportMenuBox title="TAI" eachImg="9" />
+          <ReportMenuBox title="ภาวะสมองเสื่อม" eachImg="10" />
+          <ReportMenuBox title="MMSE" eachImg="11" />
+          <ReportMenuBox title="โรคซึมเศร้า" eachImg="12" />
+          <ReportMenuBox title="สุขภาพกระดูกและกล้ามเนื้อ" eachImg="13" />
+          <ReportMenuBox title="การกลั้นปัสสาวะ" eachImg="14" />
         </div>
         <Link to={`${path}/report3`}>To report3</Link>
       </Route>
