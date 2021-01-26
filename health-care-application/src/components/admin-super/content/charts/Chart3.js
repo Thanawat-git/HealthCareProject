54dc56649db495ac49cd0aab51095fe014b2463d
Thanawat-git <buysai123gw@gmail.com>
2021-01-26 15:47:27 +0700

    add chart3 file

---
+++ health-care-application/src/components/admin-super/content/charts/Chart3.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Chart3() {
+    return (
+        <div>
+            Chart3
+        </div>
+    )
+}
--- health-care-application/src/components/admin-super/content/charts/index.js
+++ health-care-application/src/components/admin-super/content/charts/index.js
@@ -1,6 +1,8 @@
 import Chart1 from "./Chart1";
 import Chart2 from "./Chart2";
+import Chart3 from "./Chart3";
 import Chart4 from "./Chart4";
 import Chart5 from "./Chart5";
+// import Chart6 from "./Chart6";
 
-export {Chart1, Chart2, Chart4, Chart5};
+export {Chart1, Chart2, Chart3, Chart4, Chart5};
