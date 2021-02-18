a3e9dc91ee5f30918e3fa97161c02244c2f62779
Thanawat-git <buysai123gw@gmail.com>
2021-02-18 11:58:23 +0700

    show no data

--- health-care-application/src/components/admin-super/content/eldery/ShowElderyInfo.js
+++ health-care-application/src/components/admin-super/content/eldery/ShowElderyInfo.js
@@ -17,7 +17,7 @@ import Tabs from "@material-ui/core/Tabs";
 import Tab from "@material-ui/core/Tab";
 import Typography from "@material-ui/core/Typography";
 import Box from "@material-ui/core/Box";
-import NoDataImg from '../../../images/no_data.svg'
+import NoDataImg from "../../../images/no_data.svg";
 import "moment/locale/th";
 
 // use css from content.css
@@ -121,11 +121,10 @@ export default function ShowElderyInfo({ data }) {
             <BasicInfo data={data} />
           </TabPanel>
           <TabPanel value={value} index={1} className={classes.root}>
-
             <Address data={data} />
           </TabPanel>
           <TabPanel value={value} index={2} className={classes.root}>
-           <Relative1 data={data} />             
+            <Relative1 data={data} />
           </TabPanel>
           <TabPanel value={value} index={3} className={classes.root}>
             <Relative2 data={data} />
@@ -157,9 +156,9 @@ function BasicInfo({ data }) {
         </h5>
       </div>
       {/* ------------------------ */}
-      <br/>
+      <br />
       <div className="row">
-        <div className="col-6 basic-info-title" style={{ paddingRight:0 }}>
+        <div className="col-6 basic-info-title" style={{ paddingRight: 0 }}>
           <h5>ชื่อ - นามสกุล :</h5> <hr />
           <h5>วันเกิด :</h5> <hr />
           <h5>อายุ :</h5> <hr />
@@ -173,46 +172,56 @@ function BasicInfo({ data }) {
           <h5>สิทธิการรักษา :</h5> <hr />
           <h5>การเป็นอยู่ :</h5>
         </div>
-        <div className="col-6 align-self-start" style={{ paddingLeft:0 }}>
+        <div className="col-6 align-self-start" style={{ paddingLeft: 0 }}>
           <h5>
-          {data.Elder_Information.ELD_GENDER === "ชาย"
-            ? "นาย"
-            : data.Elder_Information.ELD_STATUS === "โสด"
-            ? "นางสาว"
-            : "นาง"}
-          {data.ELD_FIRSTNAME} {data.ELD_LASTNAME}
+            {data.Elder_Information.ELD_GENDER === "ชาย"
+              ? "นาย"
+              : data.Elder_Information.ELD_STATUS === "โสด"
+              ? "นางสาว"
+              : "นาง"}
+            {data.ELD_FIRSTNAME} {data.ELD_LASTNAME}
           </h5>
-         <hr />
-         <h5>วัน{moment(data.Elder_Information.ELD_BIRTHDATE).format("dddd")} ที่ {moment(data.Elder_Information.ELD_BIRTHDATE).format("LL")}</h5> <hr/>
-         <h5>{data.Elder_Information.ELD_AGE} ปี</h5> <hr/>
-         <h5>{data.Elder_Information.ELD_GENDER}</h5> <hr/>
-         <h5>{data.Elder_Information.ELD_STATUS}</h5> <hr/>
-         <h5>{!data.ELD_PHONE?"-":data.ELD_PHONE}</h5> <hr/>
-         <h5>{data.Elder_Information.ELD_RELIGION}</h5> <hr/>
-         <h5>{data.Elder_Information.ELD_JOB}</h5> <hr/>
-         <h5>{data.Elder_Information.ELD_INCOME} บาท</h5> <hr/>
-         <h5>{data.Elder_Information.ELD_EDUCATION}</h5> <hr/>
-         <h5>
-            {data.Elder_Information.ELD_TREATMENT}{" "}
-            {data.Elder_Information.ELD_TREATMENT_HOSPITAL !== null && `โรงพยาบาล${data.Elder_Information.ELD_TREATMENT_HOSPITAL}`}
-          </h5> <hr/>
+          <hr />
           <h5>
-          {
-            data.Elder_Information.ELD_LIVELIHOOD === "ไม่ได้อยู่ลำพัง" ? `ไม่ได้อยู่ลำพัง อยู่กับ ${data.Elder_Information.ELD_LIVELIHOOD_NEIGHBOR_NAME}`
-            : data.Elder_Information.ELD_LIVELIHOOD_DETAIL === "ทั้งกลางวันและกลางคืน" ? `${data.Elder_Information.ELD_LIVELIHOOD} ${data.Elder_Information.ELD_LIVELIHOOD_DETAIL}` 
-            : data.Elder_Information.ELD_LIVELIHOOD_DETAIL === "กลางวัน" ? (
+            วัน{moment(data.Elder_Information.ELD_BIRTHDATE).format("dddd")} ที่{" "}
+            {moment(data.Elder_Information.ELD_BIRTHDATE).format("LL")}
+          </h5>{" "}
+          <hr />
+          <h5>{data.Elder_Information.ELD_AGE} ปี</h5> <hr />
+          <h5>{data.Elder_Information.ELD_GENDER}</h5> <hr />
+          <h5>{data.Elder_Information.ELD_STATUS}</h5> <hr />
+          <h5>{!data.ELD_PHONE ? "-" : data.ELD_PHONE}</h5> <hr />
+          <h5>{data.Elder_Information.ELD_RELIGION}</h5> <hr />
+          <h5>{data.Elder_Information.ELD_JOB}</h5> <hr />
+          <h5>{data.Elder_Information.ELD_INCOME} บาท</h5> <hr />
+          <h5>{data.Elder_Information.ELD_EDUCATION}</h5> <hr />
+          <h5>
+            {data.Elder_Information.ELD_TREATMENT}{" "}
+            {data.Elder_Information.ELD_TREATMENT_HOSPITAL !== null &&
+              `โรงพยาบาล${data.Elder_Information.ELD_TREATMENT_HOSPITAL}`}
+          </h5>{" "}
+          <hr />
+          <h5>
+            {data.Elder_Information.ELD_LIVELIHOOD === "ไม่ได้อยู่ลำพัง" ? (
+              `ไม่ได้อยู่ลำพัง อยู่กับ ${data.Elder_Information.ELD_LIVELIHOOD_NEIGHBOR_NAME}`
+            ) : data.Elder_Information.ELD_LIVELIHOOD_DETAIL ===
+              "ทั้งกลางวันและกลางคืน" ? (
+              `${data.Elder_Information.ELD_LIVELIHOOD} ${data.Elder_Information.ELD_LIVELIHOOD_DETAIL}`
+            ) : data.Elder_Information.ELD_LIVELIHOOD_DETAIL === "กลางวัน" ? (
               <React.Fragment>
-                {data.Elder_Information.ELD_LIVELIHOOD} {data.Elder_Information.ELD_LIVELIHOOD_DETAIL} <br/>
-                กลางคืนอยู่กับ {data.Elder_Information.ELD_LIVELIHOOD_NEIGHBOR_NAME}
+                {data.Elder_Information.ELD_LIVELIHOOD}{" "}
+                {data.Elder_Information.ELD_LIVELIHOOD_DETAIL} <br />
+                กลางคืนอยู่กับ{" "}
+                {data.Elder_Information.ELD_LIVELIHOOD_NEIGHBOR_NAME}
               </React.Fragment>
-            )
-            : (
+            ) : (
               <React.Fragment>
-                {data.Elder_Information.ELD_LIVELIHOOD} {data.Elder_Information.ELD_LIVELIHOOD_DETAIL} <br/>
-                กลางวันอยู่กับ {data.Elder_Information.ELD_LIVELIHOOD_NEIGHBOR_NAME}
+                {data.Elder_Information.ELD_LIVELIHOOD}{" "}
+                {data.Elder_Information.ELD_LIVELIHOOD_DETAIL} <br />
+                กลางวันอยู่กับ{" "}
+                {data.Elder_Information.ELD_LIVELIHOOD_NEIGHBOR_NAME}
               </React.Fragment>
-            )
-          }
+            )}
           </h5>
         </div>
       </div>
@@ -244,8 +253,9 @@ function Address({ data }) {
       <div className="address-info">
         <p style={{ fontSize: 20, margin: 20 }}>
           &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;เลขที่ :&nbsp; {ELD_CUR_NUMBER}{" "}
-          &nbsp;ตรอก/ซอย :&nbsp; {ELD_CUR_ALLEY} &nbsp;ถนน :&nbsp; {ELD_CUR_STREET}{" "}
-          {ELD_CUR_AREA} &nbsp;ตำบล :&nbsp; {ELD_CUR_SUB_DISTRICT} &nbsp;
+          &nbsp;ตรอก/ซอย :&nbsp; {ELD_CUR_ALLEY} &nbsp;ถนน :&nbsp;{" "}
+          {ELD_CUR_STREET} {ELD_CUR_AREA} &nbsp;ตำบล :&nbsp;{" "}
+          {ELD_CUR_SUB_DISTRICT} &nbsp;
           <br /> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;อำเภอเมือง จังหวัดชลบุรี
         </p>
       </div>
@@ -269,71 +279,83 @@ function Address({ data }) {
 }
 
 function Relative1({ data }) {
-  const {
-    ELDER_RELATIVE
-  } = data;
+  const { ELDER_RELATIVE } = data;
 
   return (
     <React.Fragment>
-      {(ELDER_RELATIVE.length !== 0 && ELDER_RELATIVE[0].ELD_REL_FIRSTNAME !== null )  ?
-      <React.Fragment>
-      <h5 style={{ marginLeft: "7%" }}>
-        <RecentActorsSharpIcon style={{ color: grey[500], fontSize: 40 }} />
-        &nbsp;<strong>ข้อมูลญาติคนที่ 1</strong>
-      </h5>
-      <div style={{ marginLeft: "17%", fontSize: 20 }}>
-        <p>
-          ชื่อ-นามสกุล : &nbsp;{ELDER_RELATIVE[0].ELD_REL_FIRSTNAME} &nbsp; {ELDER_RELATIVE[0].ELD_REL_LASTNAME}
-        </p>
-        <p>เพศ : &nbsp;{ELDER_RELATIVE[0].ELD_REL_GENDER} </p>
-        <p>เวลาอยู่กับผู้สูงอายุ : &nbsp;{ELDER_RELATIVE[0].ELD_REL_TIME}</p>
-        <p>ความสัมพันธ์กับผู้สูงอายุ : &nbsp;{ELDER_RELATIVE[0].ELD_REL_RELATION}</p>
-        <p>เบอร์โทรศัพท์ : &nbsp;{ELDER_RELATIVE[0].ELD_REL_PHONE}</p>
-      </div>
-      </React.Fragment>
-      :<p style={{fontSize: 20 }}>ไม่มีข้อมูลญาติ</p>
-      }
+      {ELDER_RELATIVE.length !== 0 &&
+      ELDER_RELATIVE[0].ELD_REL_FIRSTNAME !== null ? (
+        <React.Fragment>
+          <h5 style={{ marginLeft: "7%" }}>
+            <RecentActorsSharpIcon style={{ color: grey[500], fontSize: 40 }} />
+            &nbsp;<strong>ข้อมูลญาติคนที่ 1</strong>
+          </h5>
+          <div style={{ marginLeft: "17%", fontSize: 20 }}>
+            <p>
+              ชื่อ-นามสกุล : &nbsp;{ELDER_RELATIVE[0].ELD_REL_FIRSTNAME} &nbsp;{" "}
+              {ELDER_RELATIVE[0].ELD_REL_LASTNAME}
+            </p>
+            <p>เพศ : &nbsp;{ELDER_RELATIVE[0].ELD_REL_GENDER} </p>
+            <p>
+              เวลาอยู่กับผู้สูงอายุ : &nbsp;{ELDER_RELATIVE[0].ELD_REL_TIME}
+            </p>
+            <p>
+              ความสัมพันธ์กับผู้สูงอายุ : &nbsp;
+              {ELDER_RELATIVE[0].ELD_REL_RELATION}
+            </p>
+            <p>เบอร์โทรศัพท์ : &nbsp;{!ELDER_RELATIVE[0].ELD_REL_PHONE?"-":ELDER_RELATIVE[0].ELD_REL_PHONE}</p>
+          </div>
+        </React.Fragment>
+      ) : (
+        <div className="row justify-content-center nodata-display">
+          <div className="col align-self-start"></div>
+          <div className="col align-self-center">
+            <img src={NoDataImg} className="nodata-img" alt="" />
+            <span>ไม่พบข้อมูล</span>
+          </div>
+          <div className="col align-self-end"></div>
+        </div>
+      )}
     </React.Fragment>
   );
 }
 function Relative2({ data }) {
-  const {
-    ELDER_RELATIVE
-  } = data;
+  const { ELDER_RELATIVE } = data;
 
   return (
     <React.Fragment>
-      {ELDER_RELATIVE.length > 1 ?
-      <React.Fragment> 
-      <h5 style={{ marginLeft: "7%" }}>
-        <RecentActorsSharpIcon style={{ color: grey[500], fontSize: 40 }} />
-        &nbsp;<strong>ข้อมูลญาติคนที่ 2</strong>
-      </h5>
-      <div style={{ marginLeft: "17%", fontSize: 20 }}>
-        <p>
-          ชื่อ-นามสกุล : &nbsp;{ELDER_RELATIVE[1].ELD_REL_FIRSTNAME} &nbsp; {ELDER_RELATIVE[1].ELD_REL_LASTNAME}
-        </p>
-        <p>เพศ : &nbsp;{ELDER_RELATIVE[1].ELD_REL_GENDER} </p>
-        <p>เวลาอยู่กับผู้สูงอายุ : &nbsp;{ELDER_RELATIVE[1].ELD_REL_TIME}</p>
-        <p>ความสัมพันธ์กับผู้สูงอายุ : &nbsp;{ELDER_RELATIVE[1].ELD_REL_RELATION}</p>
-        <p>เบอร์โทรศัพท์ : &nbsp;{ELDER_RELATIVE[1].ELD_REL_PHONE}</p>
-      </div>
-      </React.Fragment> 
-      : 
-      <div className="row justify-content-center nodata-display">
-        <div className="col align-self-start">
-          {/* One of three columns */}
+      {ELDER_RELATIVE.length > 1 ? (
+        <React.Fragment>
+          <h5 style={{ marginLeft: "7%" }}>
+            <RecentActorsSharpIcon style={{ color: grey[500], fontSize: 40 }} />
+            &nbsp;<strong>ข้อมูลญาติคนที่ 2</strong>
+          </h5>
+          <div style={{ marginLeft: "17%", fontSize: 20 }}>
+            <p>
+              ชื่อ-นามสกุล : &nbsp;{ELDER_RELATIVE[1].ELD_REL_FIRSTNAME} &nbsp;{" "}
+              {ELDER_RELATIVE[1].ELD_REL_LASTNAME}
+            </p>
+            <p>เพศ : &nbsp;{ELDER_RELATIVE[1].ELD_REL_GENDER} </p>
+            <p>
+              เวลาอยู่กับผู้สูงอายุ : &nbsp;{ELDER_RELATIVE[1].ELD_REL_TIME}
+            </p>
+            <p>
+              ความสัมพันธ์กับผู้สูงอายุ : &nbsp;
+              {ELDER_RELATIVE[1].ELD_REL_RELATION}
+            </p>
+            <p>เบอร์โทรศัพท์ : &nbsp;{!ELDER_RELATIVE[1].ELD_REL_PHONE?"-":ELDER_RELATIVE[1].ELD_REL_PHONE}</p>
+          </div>
+        </React.Fragment>
+      ) : (
+        <div className="row justify-content-center nodata-display">
+          <div className="col align-self-start"></div>
+          <div className="col align-self-center">
+            <img src={NoDataImg} className="nodata-img" alt="" />
+            <span>ไม่พบข้อมูล</span>
+          </div>
+          <div className="col align-self-end"></div>
         </div>
-        <div className="col align-self-center">
-          <img src={NoDataImg} className="nodata-img" alt=""/>
-          <span>ไม่พบข้อมูล</span>
-        </div>
-        <div className="col align-self-end">
-          {/* One of three columns */}
-        </div>
-      </div>
-    
-}
+      )}
     </React.Fragment>
   );
 }
@@ -348,7 +370,7 @@ function CongenitalDisease({ data }) {
           <h5 style={{ marginTop: "3%" }}>
             <strong>ประวัติการแพ้อาหาร</strong>
           </h5>
-          {FOOD.length !== 0 ?
+          {FOOD.length !== 0 ? (
             FOOD.map((value) => {
               return (
                 <React.Fragment>
@@ -357,13 +379,16 @@ function CongenitalDisease({ data }) {
                   </li>
                 </React.Fragment>
               );
-            }):<p style={{fontSize: 18,marginLeft:"6%" }}>ไม่มี</p>}
+            })
+          ) : (
+            <p style={{ fontSize: 18, marginLeft: "6%" }}>ไม่มี</p>
+          )}
         </div>
         <div className="col-4">
           <h5 style={{ marginTop: "3%" }}>
             <strong>ประวัติการแพ้ยา</strong>
           </h5>
-          {DRUG.length !== 0 ? 
+          {DRUG.length !== 0 ? (
             DRUG.map((value, index) => {
               return (
                 <React.Fragment>
@@ -372,13 +397,16 @@ function CongenitalDisease({ data }) {
                   </li>
                 </React.Fragment>
               );
-            }):<p style={{fontSize: 18,marginLeft:"6%" }}>ไม่มี</p>}
+            })
+          ) : (
+            <p style={{ fontSize: 18, marginLeft: "6%" }}>ไม่มี</p>
+          )}
         </div>
         <div className="col-4">
           <h5 style={{ marginTop: "3%" }}>
             <strong>โรคประจำตัว</strong>
           </h5>
-          {DISEASE.length !== 0 ?
+          {DISEASE.length !== 0 ? (
             DISEASE.map((value) => {
               return (
                 <React.Fragment>
@@ -387,7 +415,10 @@ function CongenitalDisease({ data }) {
                   </li>
                 </React.Fragment>
               );
-            }):<p style={{fontSize: 18,marginLeft:"6%" }}>ไม่มี</p>}
+            })
+          ) : (
+            <p style={{ fontSize: 18, marginLeft: "6%" }}>ไม่มี</p>
+          )}
         </div>
       </div>
     </React.Fragment>
