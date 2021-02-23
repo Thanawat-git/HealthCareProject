9a048e9c913ef5ab78ae0443419303e26939574c
Thanawat-git <buysai123gw@gmail.com>
2021-02-23 11:55:40 +0700

    Update index.js

--- health-care-application/src/components/admin-super/content/reports/index.js
+++ health-care-application/src/components/admin-super/content/reports/index.js
@@ -56,12 +56,12 @@ export default function Index() {
           <ReportMenuBox title="สุขภาพตา" eachImg="6" />
           <ReportMenuBox title="สุขภาพช่องปาก" eachImg="7" />
           <ReportMenuBox title="ความสามารถในการทำกิจวัตรประจำวัน" eachImg="8" />
-          <ReportMenuBox title="TAI" eachImg="9" />
+          {/* <ReportMenuBox title="TAI" eachImg="9" /> */}
           <ReportMenuBox title="ภาวะสมองเสื่อม" eachImg="10" />
-          <ReportMenuBox title="MMSE" eachImg="11" />
+          {/* <ReportMenuBox title="MMSE" eachImg="11" /> */}
           <ReportMenuBox title="โรคซึมเศร้า" eachImg="12" />
           <ReportMenuBox title="สุขภาพกระดูกและกล้ามเนื้อ" eachImg="13" />
-          <ReportMenuBox title="การกลั้นปัสสาวะ" eachImg="14" />
+          {/* <ReportMenuBox title="การกลั้นปัสสาวะ" eachImg="14" /> */}
         </div>
         <Link to={`${path}/report3`}>To report3</Link>
       </Route>
